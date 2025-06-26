011eca2ab04935bdbe4c965351894180058ac3fd
ebuxtain <ebuxtain@gmail.com>
2025-06-26 15:20:55 +0100

    Fix case-sensitive import path for Content.jsx

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import Content from "./Content";
+import Content from "./content";
 import Extras from "./Extras";
 
 const Layout = () => (
